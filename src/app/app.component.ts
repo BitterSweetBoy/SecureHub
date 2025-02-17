9ee4e2512d2826e0028dbc5b55c3b734c793e9f8
Denis Dueñas <denis.mejia@cit.hn>
2025-02-16 18:57:56 -0600

    Agregando PrimeNg + Sakai Template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Router, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -9,4 +9,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'SecureHub';
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Apply 'login-mode' if we are at '/',
+        const isLogin = event.url === '/';
+        document.body.className = isLogin ? 'login-mode' : 'app-mode';
+      }
+    });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppLayout } from './layout/component/app.layout';
+import { UserComponent } from './user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+         path: 'layout', component: AppLayout 
+    }
+];
